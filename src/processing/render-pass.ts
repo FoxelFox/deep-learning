18241319ec45b817af6961511522441e458ec57f
André Furchner <storkner@gmail.com>
2016-12-29 22:17:08 +0100

    double buffering

--- src/processing/render-pass.ts
+++ src/processing/render-pass.ts
@@ -2,31 +2,45 @@ import * as THREE from "three";
 import {render} from "./render";
 
 export class RenderPass {
-	private shader: any; // TODO
+	private shader: THREE.ShaderMaterial;
 	private scene: THREE.Scene;
 	private camera: THREE.Camera;
-	private target: THREE.WebGLRenderTarget;
+	private targets: THREE.WebGLRenderTarget[];
 	private quad: THREE.Object3D;
+  	private targetIndex: number = 0;
 
 	constructor(width: number, height: number) {
-		this.target = new THREE.WebGLRenderTarget(width, height);
+		this.targets = [
+			new THREE.WebGLRenderTarget(width, height),
+			new THREE.WebGLRenderTarget(width, height)
+		];
+
 		this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
 		this.scene = new THREE.Scene();
 
 		// create quad
 		let geometry = new THREE.PlaneGeometry( 2, 2);
-		let material = new THREE.ShaderMaterial({
+		this.shader = new THREE.ShaderMaterial({
+			uniforms: {
+				map: { value: this.targets[1].texture }
+			},
 			vertexShader: require("./shader/basic.vertex.glsl"),
 			fragmentShader: require("./shader/basic.fragment.glsl")
-		})
-		this.quad = new THREE.Mesh( geometry, material );
-		
+		});
+
+		this.quad = new THREE.Mesh( geometry, this.shader );
+
 		this.scene.add(this.quad);
 
 	}
 
 	public process() {
-		render.render(this.scene, this.camera, this.target);
+		render.render(this.scene, this.camera, this.targets[this.targetIndex]);
+
+		// swap targets
+		this.shader.uniforms["map"].value = this.targets[this.targetIndex].texture;
+		this.targetIndex = this.targetIndex ? 0 : 1;
+
 	}
 
 	public processOnCanvas() {
@@ -36,6 +50,6 @@ export class RenderPass {
 	}
 
 	get Output(): THREE.Texture {
-		return this.target.texture;
+		return this.targets[0].texture;
 	}
 }
--- src/processing/shader/basic.fragment.glsl
+++ src/processing/shader/basic.fragment.glsl
@@ -1,3 +1,6 @@
+uniform sampler2D map;
+varying vec2 v_uv;
 void main() {
-	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	vec4 self = texture2D(map, v_uv);
+	gl_FragColor = vec4(self.x +0.5, 0.0, 0.0, 1.0);
 }
